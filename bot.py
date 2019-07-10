837c70cbf8d23da98b253d4fcc8415648fa3b899
Ivan Lavrenov <dev5@topnomer.ru>
2019-07-10 20:39:06 +0300

    added old methods

--- bot.py
+++ bot.py
@@ -1,86 +1,12 @@
-import telebot
 import time
+
+import telebot
 import flask
 
 import settings
 import movie_funcs
-from .movie_funcs import getMovieById
-from .handful_funcs import showListOfMovies, fullDescOfMovie, compareQueryAndMovies
+from handful_funcs import showListOfMovies, fullDescOfMovie, compareQueryAndMovies
 
-
-"""
-def search_movies(message):
-    if message.text == '/exit':
-        return True
-    searchQuery = message.text
-    movies = movie_funcs.getMoviesListByQuery(searchQuery, MOVIEDB_TOKEN)
-
-    # if query doesn't have any correlations with existing movies. EXIT
-    if len(movies) == 0:
-        msg = bot.reply_to(message, 'I don\'t know a single movie with this title. Try again or type /exit to exit')
-        bot.register_next_step_handler(msg, search_movies)
-        return True
-
-    # if 1 or more movie has been found
-    elif len(movies) >= 1:
-
-        # Handle choosing from list of movies
-        def choiceFromList(message):
-            nonlocal movies
-            text = message.text
-            # Checking msg for digitable
-            if text.isdigit():
-                # Handle if number not in list. EXIT
-                if int(text) > len(movies) or int(text) < 1:
-                    bot.reply_to(message, 'ERROR. Your number not in list :(\n Select right number from list')
-                    bot.register_next_step_handler(message, choiceFromList)
-                # If in list..
-                else:
-                    movie = movies[int(text) - 1]
-                    image = movie_funcs.getPosterById(movie['id'], MOVIEDB_TOKEN, IMAGE_BASE_URL)
-                    bot.send_photo(message.chat.id, image)
-                    msg = bot.send_message(message.chat.id, fullDescOfMovie(getMovieById(movie['id'], MOVIEDB_TOKEN)),
-                                           parse_mode="HTML")
-                    bot.register_next_step_handler(msg, search_movies)
-                    return True
-            # If not a digit. RETURN TO CHOICE
-            else:
-                bot.reply_to(message, 'ERROR. Please, input number of the movie')
-                bot.register_next_step_handler(message, choiceFromList)
-
-        result = compareQueryAndMovies(searchQuery, movies)
-        # if only one movie with same name as query exist
-        if result:
-            image = movie_funcs.getPosterById(result['id'], MOVIEDB_TOKEN, IMAGE_BASE_URL)
-            bot.send_photo(message.chat.id, image)
-            msg = bot.send_message(message.chat.id, fullDescOfMovie(getMovieById(result['id'], MOVIEDB_TOKEN)),
-                                   parse_mode="HTML")
-            bot.register_next_step_handler(msg, search_movies)
-            return True
-        else:
-            bot.send_message(message.chat.id, '''
-                Has been found more than 1 movie with this title.\n
-                Enter number of movie or /exit to exit 
-            ''')
-            msg = bot.reply_to(message, showListOfMovies(movies))
-            bot.register_next_step_handler(msg, choiceFromList)
-
-
-@bot.message_handler(commands=['search', ])
-def search(message):
-    msg = bot.reply_to(message, 'send me what you want to search')
-    bot.register_next_step_handler(msg, search_movies)
-
-
-@bot.message_handler(commands=['poster', ])
-def posterPost(message):
-    msg = message.text.replace('/poster ', '')
-    if not msg.isdigit():
-        bot.reply_to(message, 'Please, enter id of the movie!')
-    image = movie_funcs.getPosterById(msg, MOVIEDB_TOKEN, IMAGE_BASE_URL)
-    print('image has been received!')
-    bot.send_photo(message.chat.id, image)
-"""
 bot = telebot.TeleBot(settings.TOKEN)
 server = flask.Flask(__name__)
 
@@ -110,16 +36,77 @@ def start(message):
     bot.reply_to(message, reply)
 
 
-bot.remove_webhook()
-time.sleep(0.1)
+def search_movies(message):
+    if message.text == '/exit':
+        return True
+    searchQuery = message.text
+    movies = movie_funcs.getMoviesListByQuery(searchQuery, settings.MOVIEDB_TOKEN)
+
+    # if query doesn't have any correlations with existing movies. EXIT
+    if len(movies) == 0:
+        msg = bot.reply_to(message, 'I don\'t know a single movie with this title. Try again or type /exit to exit')
+        bot.register_next_step_handler(msg, search_movies)
+        return True
+
+    # if 1 or more movie has been found
+    elif len(movies) >= 1:
+
+        # Handle choosing from list of movies
+        def choiceFromList(message):
+            nonlocal movies
+            text = message.text
+            # Checking msg for digitable
+            if text.isdigit():
+                # Handle if number not in list. EXIT
+                if int(text) > len(movies) or int(text) < 1:
+                    bot.reply_to(message, 'ERROR. Your number not in list :(\n Select right number from list')
+                    bot.register_next_step_handler(message, choiceFromList)
+                # If in list..
+                else:
+                    movie = movies[int(text) - 1]
+                    image = movie_funcs.getPosterById(movie['id'], settings.MOVIEDB_TOKEN, settings.IMAGE_BASE_URL)
+                    bot.send_photo(message.chat.id, image)
+                    msg = bot.send_message(message.chat.id, fullDescOfMovie(movie_funcs.getMovieById(movie['id'],
+                                                                                                     settings.MOVIEDB_TOKEN)),
+                                           parse_mode="HTML")
+                    bot.register_next_step_handler(msg, search_movies)
+                    return True
+            # If not a digit. RETURN TO CHOICE
+            else:
+                bot.reply_to(message, 'ERROR. Please, input number of the movie')
+                bot.register_next_step_handler(message, choiceFromList)
+
+        result = compareQueryAndMovies(searchQuery, movies)
+        # if only one movie with same name as query exist
+        if result:
+            image = movie_funcs.getPosterById(result['id'], settings.MOVIEDB_TOKEN, settings.IMAGE_BASE_URL)
+            bot.send_photo(message.chat.id, image)
+            msg = bot.send_message(message.chat.id, fullDescOfMovie(movie_funcs.getMovieById(result['id'],
+                                                                                             settings.MOVIEDB_TOKEN)),
+                                   parse_mode="HTML")
+            bot.register_next_step_handler(msg, search_movies)
+            return True
+        else:
+            bot.send_message(message.chat.id, '''
+                Has been found more than 1 movie with this title.\n
+                Enter number of movie or /exit to exit 
+            ''')
+            msg = bot.reply_to(message, showListOfMovies(movies))
+            bot.register_next_step_handler(msg, choiceFromList)
 
 
-bot.set_webhook(url=settings.WEBHOOK_URL_BASE + settings.WEBHOOK_URL_PATH,
-                certificate=open(settings.WEBHOOK_SSL_CERT, 'r'))
+@bot.message_handler(commands=['search', ])
+def search(message):
+    msg = bot.reply_to(message, 'send me what you want to search')
+    bot.register_next_step_handler(msg, search_movies)
 
 
-server.run(host="0.0.0.0",
-           port=settings.WEBHOOK_PORT,
-           ssl_context=(settings.WEBHOOK_SSL_CERT, settings.WEBHOOK_SSL_PRIV),
-           debug=True
-           )
+if __name__ == '__main__':
+    bot.remove_webhook()
+    time.sleep(0.1)
+    bot.set_webhook(url=settings.WEBHOOK_URL_BASE + settings.WEBHOOK_URL_PATH,
+                    certificate=open(settings.WEBHOOK_SSL_CERT, 'r'))
+    server.run(host="0.0.0.0",
+               port=settings.WEBHOOK_PORT,
+               ssl_context=(settings.WEBHOOK_SSL_CERT, settings.WEBHOOK_SSL_PRIV),
+               debug=True)
